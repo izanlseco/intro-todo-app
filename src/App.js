fa8600a2dabff653f704b006cb67651a47eadb5a
Izan <IzanLS09@gmail.com>
2020-08-04 14:54:42 +0200

    adding intro to react + hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from 'react';
 
-function App() {
+// const App = () => {
+//   const [name, setName] = useState('Izan');
+//   const onNameChange = useCallback((event) => {
+//     console.log(event.target.value);
+//     setName(event.target.value);
+//   }, []);
+
+//   return (
+//     <div>
+//       <form>
+//         <label>Enter your name:</label>
+//         <input
+//           value={name}
+//           onChange={onNameChange}
+//         />
+//       </form>
+//       <h1>Hello {name}!</h1>
+//     </div>
+//   );
+// };
+
+const App = () => {
+  const [newTodo, setNewTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const onNewTodoChange = useCallback((event) => {
+    setNewTodo(event.target.value);
+  }, []);
+
+  const formSubmitted = useCallback((event) => {
+    event.preventDefault();
+    if (!newTodo.trim()) return;
+    setTodos([
+      {
+        id: todos.length ? todos[0].id + 1 : 1,
+        content: newTodo,
+        done: false,
+      },
+      ...todos
+    ]);
+    setNewTodo('');
+  }, [newTodo, todos]);
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  const addTodo = useCallback((todo, index) => (event) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1, {
+      ...todo,
+      done: !todo.done
+    });
+    setTodos(newTodos);
+  }, [todos]);
+
+  const removeTodo = useCallback((todo) => (event) => {
+    setTodos(todos.filter(otherTodo => otherTodo !== todo));
+  }, [todos]);
+
+  const markAllDone = useCallback(() => {
+    const updatedTodos = todos.map(todo => {
+      return {
+        ...todo,
+        done: true,
+      };
+    });
+    setTodos(updatedTodos);
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={formSubmitted}>
+        <label htmlFor="newTodo">Enter a Todo:</label>
+        <input
+          id="newTodo"
+          name="newTodo"
+          value={newTodo}
+          onChange={onNewTodoChange}
+        />
+        <button>Add Todo</button>
+      </form>
+      <button onClick={markAllDone}>Mark all done</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={todo.id}>
+            <input
+              checked={todo.done}
+              type="checkbox"
+              onChange={addTodo(todo, index)}
+            />
+            <span className={todo.done ? 'done' : ''}>{todo.content}</span>
+            <button onClick={removeTodo(todo)}>Remove Todo</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
